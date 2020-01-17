2ab0a8abf8c8b4aa6000cb2cc4ffbcc4d279f813
Tarun-P <46513155+Tarun-P@users.noreply.github.com>
2020-01-17 14:14:31 -0500

    Loading Screen

---
+++ Laika Game/Assets/Loading.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    private bool loadScene = false;
+    private bool oneInvoke = true;
+    [SerializeField]
+    private int scene;
+    [SerializeField]
+    private Text loadingText;
+    public GameObject ImagesParent;
+    private Image image;
+    private int childIndex = 0;
+    private int numOfImages;
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text text;
+    public GameObject spaceNoise;
+    public GameObject laikaRun;
+
+    private void Start()
+    {
+        Transform[] allChildren = ImagesParent.GetComponentsInChildren<Transform>();
+        numOfImages = allChildren.Length-1;
+        print(numOfImages);
+        for(int i = 1; i < allChildren.Length - 1; i++)
+        {
+            ImagesParent.transform.GetChild(i).GetComponent<Image>().CrossFadeAlpha(0, 0, false);
+        }
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Space) && loadScene == false)
+        {
+            loadScene = true;
+            loadingText.text = "Loading...";
+            StartCoroutine(LoadNewScene());
+        }
+
+        if (loadScene == true)
+        {
+            spaceNoise.SetActive(true);
+            laikaRun.SetActive(true);
+            loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
+            if (oneInvoke)
+            {
+                oneInvoke = false;
+                InvokeRepeating("fadeOut", 0, 6f);
+            }
+        }
+
+    }
+
+    void fadeOut()
+    {
+        image = ImagesParent.transform.GetChild(childIndex).GetComponent<Image>();
+        image.CrossFadeAlpha(0, 2f, false);
+        Invoke("fadeIn", 2.0f);
+
+    }
+
+    void fadeIn()
+    {
+        childIndex = (childIndex + 1) % numOfImages;
+        
+        image = ImagesParent.transform.GetChild(childIndex).GetComponent<Image>();
+        print("Fading in " + image.name);
+        image.CrossFadeAlpha(1, 2.0f, false);
+    }
+
+    IEnumerator LoadNewScene()
+    {
+
+        loadingScreen.SetActive(true);
+        yield return new WaitForSeconds(3);
+        slider.value = 0.33f;
+        text.text = "33%";
+        yield return new WaitForSeconds(3);
+        text.text = "66%";
+        slider.value = 0.66f;
+        yield return new WaitForSeconds(3);
+        slider.value = 0.93f;
+        text.text = "93%";
+        yield return new WaitForSeconds(1);
+        slider.value = 1f;
+        text.text = "100%";
+        AsyncOperation async = SceneManager.LoadSceneAsync(scene);
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+    }
+
+}
