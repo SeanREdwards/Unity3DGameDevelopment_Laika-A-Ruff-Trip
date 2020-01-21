0284fdd494dad5edfa9811db27876802292f52cf
Tarun-P <46513155+Tarun-P@users.noreply.github.com>
2020-01-21 16:45:59 -0500

    Fixed fade

---
+++ Laika Game/Assets/_Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float xRotate;
+    public float yRotate;
+    public float zRotate;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(xRotate, yRotate, zRotate) * Time.deltaTime);
+    }
+}
