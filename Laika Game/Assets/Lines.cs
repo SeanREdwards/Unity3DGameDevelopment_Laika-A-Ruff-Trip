9541173a488039224c502cab974452f5016e5df3
Tarun-P <46513155+Tarun-P@users.noreply.github.com>
2020-02-04 19:18:44 -0500

    Line Renderer

---
+++ Laika Game/Assets/Lines.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lines : MonoBehaviour
+{
+    public GameObject lineOrigin;
+    public GameObject pickup;
+    private Vector3 originLocation;
+    private Vector3 pickupLocation;
+    LineRenderer lineRenderer;
+    public GameObject line;
+    public Vector3[] points;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = line.GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<PlayerObjectMovement>().ball != null)
+        {
+            pickup = player.GetComponent<PlayerObjectMovement>().ball;
+            originLocation = lineOrigin.transform.position;
+            pickupLocation = pickup.transform.position;
+            points[0] = originLocation;
+            points[1] = pickupLocation;
+            lineRenderer.SetPositions(points);
+            lineRenderer.enabled = true;
+        } else
+        {
+            lineRenderer.enabled = false;
+        }
+    }
+}
