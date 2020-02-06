f34c1e72f491d5995787e8c099da29a4770693be
jhelca1198 <47307902+jhelca1198@users.noreply.github.com>
2020-02-06 03:09:13 -0500

    tb2

---
+++ Laika Game/Assets/_Scripts/HasDialogue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HasDialogue : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject Player;
+    public GameObject NPC;
+    public GameObject Text;
+    public float TalkDistance;
+    private float d;
+    
+    
+    public void TriggerDialogue() {
+        Text.SetActive(false);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void ContinueDialogue() {
+        FindObjectOfType<DialogueManager>().DisplayNextSentence();
+    }
+
+    void Start()
+    {
+        d = Vector3.Distance(Player.transform.position, NPC.transform.position);
+        Text.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        d = Vector3.Distance(Player.transform.position, NPC.transform.position);
+        if (d < TalkDistance) {
+            if (!FindObjectOfType<DialogueManager>().IsTalking()) {
+                Text.SetActive(true);
+                if (Input.GetButtonDown("Interact")) {
+                    
+                    TriggerDialogue();
+                }
+            } else {
+                if (Input.GetButtonDown("Select")) {
+                    ContinueDialogue();
+                }
+            }
+            
+        } else {
+            Text.SetActive(false);
+        }
+    }
+}
---
+++ Laika Game/Assets/_Scripts/WarpScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WarpScript : MonoBehaviour
+{
+
+    public PlayerHandler Player;
+    public GameObject Door;
+    public GameObject Text;
+    public float AlertDistance;
+    private float d;
+    public int targetScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        d = Vector3.Distance(Player.transform.position, Door.transform.position);
+        Text.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        d = Vector3.Distance(Player.transform.position, Door.transform.position);
+        if (d < AlertDistance) {
+            
+            Text.SetActive(true);
+            if (Input.GetButtonDown("Interact")) {
+                Player.Save();
+                SceneManager.LoadScene(targetScene);
+            }
+           
+
+        }
+        else {
+            Text.SetActive(false);
+        }
+    }
+}
