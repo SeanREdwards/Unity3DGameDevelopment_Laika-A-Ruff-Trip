ce55098dfb7b8b7817bd917c890375b1a43e49b1
ShawnHarkins <ShawnHarkins@users.noreply.github.com>
2020-02-19 18:02:19 -0500

    AI Update + Jailed Pupper

--- Laika Game/Assets/_Scripts/EnemySight.cs
+++ Laika Game/Assets/_Scripts/EnemySight.cs
@@ -11,14 +11,15 @@ public class EnemySight : MonoBehaviour
     public GameObject sound;
     private NavMeshAgent agent;
     private INPCState CurrentBehavior;
-
+    private int PatrolReturn;
 
     // Start is called before the first frame update
     void Start()
     {
+        PatrolReturn = 0;
         agent = transform.parent.GetComponent<NavMeshAgent>();
         agent.destination = agent.transform.position;
-        CurrentBehavior = new NPCPatrolState(PatrolPoints);
+        CurrentBehavior = new NPCPatrolState(PatrolPoints,PatrolReturn);
     }
 
     // Update is called once per frame
@@ -27,22 +28,28 @@ public class EnemySight : MonoBehaviour
         CurrentBehavior.StateUpdate(agent, transform);        
     }
 
+    //Begin Chasing
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name.Equals("Laika"))
         {
             excl.SetActive(true);
             sound.SetActive(true);
+            if (CurrentBehavior is NPCPatrolState)
+            {
+                PatrolReturn = ((NPCPatrolState)CurrentBehavior).GetPatrolReturn();
+            }
             CurrentBehavior = new NPCChasingState(other.gameObject);
         }
     }
+    //Stop Chasing
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.name.Equals("Laika"))
         {
             excl.SetActive(false);
             sound.SetActive(false);
-            CurrentBehavior = new NPCPatrolState(PatrolPoints);
+            CurrentBehavior = new NPCPatrolState(PatrolPoints, PatrolReturn);
         }
     }
 
