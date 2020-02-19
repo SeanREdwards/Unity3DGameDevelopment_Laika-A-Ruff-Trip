93a99e83caa3e56e3bded546d87d7200d87c2924
SeanREdwards <edwards.897@osu.edu>
2020-02-19 14:08:56 -0500

    added cannon stuff

---
+++ Laika Game/Assets/Cannon/CannonProjectileScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonProjectileScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Destroy(gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(Resources.Load("ProjectileExplosion(Purple)"), gameObject.transform.position, gameObject.transform.rotation);
+        Destroy(gameObject);
+    }
+}
