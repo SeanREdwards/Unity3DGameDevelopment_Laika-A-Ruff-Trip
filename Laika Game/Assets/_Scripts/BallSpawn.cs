93715a9595503daca2586877487a236f4502fea2
jhelca1198 <47307902+jhelca1198@users.noreply.github.com>
2020-02-19 14:21:03 -0500

    Puzzle branch added

---
+++ Laika Game/Assets/_Scripts/BallSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallSpawn : MonoBehaviour
+{
+    public GameObject ball;
+    public Text helpText;
+
+    private Rigidbody rb;
+    void Awake() {
+        rb = ball.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Laika") {
+            helpText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "Laika") {
+            helpText.gameObject.SetActive(false);
+
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.name == "Laika") {
+            if (Input.GetKeyDown("f")) {
+                rb.velocity = new Vector3(0f, 0f, 0f);
+                rb.transform.position = new Vector3(4.7f, 8.2f, 3.5f);
+            }
+        }
+    }
+}
---
+++ Laika Game/Assets/_Scripts/LightPillarScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightPillarScript : MonoBehaviour
+{
+    public Text helpText;
+    public GameObject LightSelfRed;
+    public GameObject LightSelfGreen;
+    public GameObject LightLeftRed;
+    public GameObject LightLeftGreen;
+    public GameObject LightRightRed;
+    public GameObject LightRightGreen;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Laika") {
+            helpText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "Laika") {
+            helpText.gameObject.SetActive(false);
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.name == "Laika") {
+            if (Input.GetKeyDown("f")) {
+                LightSelfRed.SetActive(!LightSelfRed.activeSelf);
+                LightSelfGreen.SetActive(!LightSelfGreen.activeSelf);
+
+                LightLeftRed.SetActive(!LightLeftRed.activeSelf);
+                LightLeftGreen.SetActive(!LightLeftGreen.activeSelf);
+
+                LightRightRed.SetActive(!LightRightRed.activeSelf);
+                LightRightGreen.SetActive(!LightRightGreen.activeSelf);
+            }
+        }
+    }
+
+}
