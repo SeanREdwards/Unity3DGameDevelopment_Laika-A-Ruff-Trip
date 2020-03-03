4f1abda22f584bd3908392851b2d3fddcc672427
Ellen Khau <ellen.khau@gmail.com>
2020-03-02 20:16:33 -0500

    fixing grid

---
+++ Laika Game/Assets/_Scripts/customGrid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customGrid : MonoBehaviour
+{
+    [SerializeField]
+    private float size = 1.0f;
+
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        float xCount, yCount, zCount;
+        position -= transform.position;
+        xCount = Mathf.RoundToInt(position.x / size);
+        yCount = Mathf.RoundToInt(position.y / size);
+        zCount = Mathf.RoundToInt(position.z / size);
+
+        Vector3 result = new Vector3(
+            (float)xCount * size, (float)yCount * size, (float)zCount * size);
+
+        result += transform.position;
+
+        return result;
+    }
+
+}
