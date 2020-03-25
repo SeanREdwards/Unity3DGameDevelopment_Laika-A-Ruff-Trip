4dd02e199e8b1ec6840cf94aa5dae00f14632c02
Ellen Khau <ellen.khau@gmail.com>
2020-03-25 12:36:54 -0400

    resolution dropdown

--- Laika Game/Assets/_Scripts/SettingsMenu.cs
+++ Laika Game/Assets/_Scripts/SettingsMenu.cs
@@ -3,28 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
     Resolution[] resolutions;
-    public Dropdown resolutionDropdown;
+    public TMP_Dropdown resolutionDropdown;
 
     void Start()
     {
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
 
+        int currentResolutionIndex = 0;
         List<string> options = new List<string>();
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.width 
+                && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
-        resolutionDropdown.AddOptions(options); //list
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
 
+    }
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetVolume(float vol) {
