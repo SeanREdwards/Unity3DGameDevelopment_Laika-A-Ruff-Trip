fe5f64abe334b3a0cb21855b58e1b9f0ac04eac0
jhelca1198 <47307902+jhelca1198@users.noreply.github.com>
2020-04-14 15:01:34 -0400

    init

---
+++ Laika Game/Assets/_Scripts/BallButtonMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallButtonMove : MonoBehaviour
+{
+    public Text helpText;
+    public Block Block;
+    Animator BlockAnimator;
+
+    private void Awake() {
+        BlockAnimator = Block.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Laika") {
+            helpText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "Laika") {
+            helpText.gameObject.SetActive(false);
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.name == "Laika") {
+            if (Input.GetKeyDown("f")) {
+                if (Block.IsMoved()) {
+                    Block.Move();
+                } else {
+                    Block.Return();
+                }
+            }
+        }
+    }
+}
---
+++ Laika Game/Assets/_Scripts/Block.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    public GameObject block;
+    public Animator BlockAnimator;
+    private bool moved;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlockAnimator = block.GetComponent<Animator>();
+        moved = false;
+    }
+
+    public bool IsMoved() {
+        return moved;
+    }
+ 
+    public void Move() {
+        moved = true;
+        BlockAnimator.SetTrigger("Move");
+    }
+
+    public void Return() {
+        moved = false;
+        BlockAnimator.SetTrigger("Return");
+    }
+}
